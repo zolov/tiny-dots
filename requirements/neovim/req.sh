28621459aa1d4fa504f7ee9bbaef2ca34b940f04
Igor Zolov <i.zolov@yandex.ru>
2025-06-06 08:15:54 +0300

    Add script for building nvim from source

--- requirements/neovim/req.sh
+++ requirements/neovim/req.sh
@@ -1,5 +1,37 @@
-"Installing dependecies for build from source"
+set -euo pipefail
+echo "For debugging, run with bash -x script.sh"
+echo "If anything went wrong. Run the following command in your build directory:"
+echo "rm -rf build .deps # the command 'make distclean' performs the same"
+
+if [ -f "/usr/bin/nvim" ]; then
+    echo "Nvim already installed. Remove it first: sudo apt remove neovim"
+	exit 1;
+fi
+
+if [ -z "$NEOVIM_LOCATION" ]; then
+	echo "Please set NEOVIM_LOCATION (eg /usr/bin or /usr/local/bin)to the location where you want to install neovim"
+	exit 1;
+fi
+
+echo "Installing dependecies for build from source"
 sudo apt-get install ninja-build \
      gettext libtool libtool-bin \
      autoconf automake cmake g++ \
      pkg-config unzip
+
+
+echo "Creating build directory"
+mkdir ~/build && cd "$_" || exit 1
+
+echo "Cloning neovim repository"
+git clone https://github.com/neovim/neovim.git
+
+echo "Checking out stable branch"
+git checkout stable
+
+echo "Building neovim..."
+make CMAKE_BUILD_TYPE=Release \
+     CMAKE_INSTALL_PREFIX="$NEOVIM_LOCATION" install
+
+echo "Adding neovim to PATH"
+export PATH="$NEOVIM_LOCATION:$PATH"
